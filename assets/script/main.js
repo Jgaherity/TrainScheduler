7dd3450511cca8e8a5b1a5f5b170ad073628c3be
Jacqueline Gaherity <v-jagahe@microsoft.com>
2018-09-24 15:50:39 -0700

    firebase updates & calls

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,6 @@
-
+ 
 $(document).ready(function(){
-
+ 
     var config = {
         apiKey: "AIzaSyB5_zoYC3_-ZkZUsgvktgFoM-i27lVt-Ls",
         authDomain: "trainscheduler-f957a.firebaseapp.com",
@@ -9,41 +9,78 @@ $(document).ready(function(){
         storageBucket: "trainscheduler-f957a.appspot.com",
         messagingSenderId: "676667446301"
       };
-      
+     
       firebase.initializeApp(config);
-
-    //Run Clock  
+ 
+      //create a variable to reference the database
+      var database=firebase.database();
+ 
+    //Run Clock 
     setInterval(function(){
         $('.current-time').html(moment().format('hh:mm:ss A'))
     }, 1000);
-
-    //when clicking submit: 
-    $('.submit').on('click', function(e) {
+ 
+    //initial values
+    var trainName;
+    var trainDestination;
+    var trainTime;
+    var trainFrequency;
+ 
+    //when clicking submit:
+    $('.submit').on('click', function(event) {
+        event.preventDefault();
         //console.log("test submit");
-
-        var trainName = $("#trainNameInput").val().trim();
-        var trainDestination = $("#destinationInput").val().trim();
-        var trainTime = $("#trainTimeInput").val().trim();
-        var trainFrequency = $("#frequencyInput").val().trim();
-
+ 
+        trainName = $("#trainNameInput").val().trim();
+        trainDestination = $("#destinationInput").val().trim();
+        trainTime = $("#trainTimeInput").val().trim();
+        trainFrequency = $("#frequencyInput").val().trim();
+ 
+        //check if values were grabbed
         console.log(trainName);
         console.log(trainDestination);
         console.log(trainTime);
         console.log(trainFrequency);
-
-
-        //push to firebase 
-        dataRef.ref().child('trains').push({
-            // trainName: trainName,
-            // trainDestination: trainDestination,
-            // trainTime: trainTime,
-            // trainFrequency: trainFrequency,
+ 
+        database.ref().set({
+            trainName: trainName,
+            trainDestination: trainDestination,
+            trainTime: trainTime,
+            trainFrequency: trainFrequency,
+ 
         });
-  
-        //need to post to table as well. 
-
-
+ 
+        // $("table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
+        $("table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td>");
+ 
     });
 
 
+
+
+        // // Firebase watcher + initial loader HINT: .on("value")
+         database.ref().on("value", function(snapshot) {
+ 
+             // Log everything that's coming out of snapshot
+             console.log(snapshot.val());
+             console.log(snapshot.val().trainName);
+             console.log(snapshot.val().trainDestination);
+             console.log(snapshot.val().trainTime);
+             console.log(snapshot.val().trainFrequency);
+     
+             // Change the HTML to reflect
+             $("#trainName").text(snapshot.val().trainName);
+             $("#destination").text(snapshot.val().trainDestination);
+             $("#frequency").text(snapshot.val().trainFrequency);
+             $("#minAway").text(snapshot.val().trainTime);
+     
+             // Handle the errors
+           }, function(errorObject) {
+             console.log("Errors handled: " + errorObject.code);
+         });
+     
+ 
 });
+ 
+
+ 
